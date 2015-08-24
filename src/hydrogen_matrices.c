fcaee14d5a7c41cbdddf49a2dd10a97a9e89f2ac
AndrewCRMartin <andrew@bioinf.org.uk>
2015-08-24 18:06:14 +0100

    hydrogen_matrices.c - Added CleanMultipleOccupancies() although this is no longer used as code is now included in ReadPDB()

--- src/hydrogen_matrices.c
+++ src/hydrogen_matrices.c
@@ -157,6 +157,10 @@ void FindMCDonorHAtoms(PDB *resA, PDB *stopA, PDB *resB, PDB *stopB, HBOND *hb);
 void StoreHBondingNPosition(PDB *start, PDB *stop, HBOND *hb);
 void FindMCAcceptorAtoms(PDB *resA, PDB *stopA, PDB *resB, PDB *stopB, HBOND *hb);
 void StoreHBondingCOPosition(PDB *start, PDB *stop, HBOND *hb);
+PDB *CleanMultipleOccupancies(PDB *pdb);
+PDB *RemoveAlternates(PDB *pdb);
+
+
 
 
 /************************************************************************/
@@ -263,8 +267,11 @@ BOOL CalcAndStoreHBondData(HBOND *hb, NAMES *names, FILE *out)
                if((fp2 = fopen(location, "r")) !=NULL)
                { 
                   /* create linked list of pdb file */  
-                  if((pdb = ReadPDB(fp2, &natoms)) !=NULL)
+                  if((pdb = ReadPDBAtoms(fp2, &natoms)) !=NULL)
                   {   
+/*                     pdb = CleanMultipleOccupancies(pdb); */
+                     pdb = RemoveAlternates(pdb);
+                     
                      /* strip any hydrogens present in protein domain file */
                      if((pdb2 = StripHPDB(pdb, &natoms2)) !=NULL)
                      {
@@ -1239,4 +1246,64 @@ void FindMCDonorHAtoms(PDB *resA, PDB *stopA, PDB *resB, PDB *stopB, HBOND *hb)
       }
    }
 }
+
+/************************************************************************/
+/* This is a kludge to deal with non-standard PDB files like 1dxc where
+   multiple occupancy atoms are listed in 2 groups rather than in atom
+   pairs
+*/
+PDB *CleanMultipleOccupancies(PDB *pdb)
+{
+
+   PDB *p       = NULL, 
+       *q       = NULL,
+       *r       = NULL,
+       *start   = NULL, 
+       *nextres = NULL;
+   BOOL GotPartial;
+   
+
+   /* Step through the PDB list one residue at a time                   */
+   for(start=pdb; start!=NULL; start=nextres)
+   {
+      nextres = FindNextResidue(start);
+
+      /* See if we have any partial occupancy atoms                     */
+      GotPartial = FALSE;
+      for(p=start; p!=nextres; NEXT(p))
+      {
+         if(p->altpos != ' ')
+         {
+            GotPartial = TRUE;
+            break;
+         }
+      }
+      
+      if(GotPartial)
+      {
+         for(p=start; p!= nextres; NEXT(p))
+         {
+            /* Got first partial position                               */
+            if(p->altpos == 'A')
+            {
+               r=p;
+               for(q=p->next; q!=nextres; NEXT(q))
+               {
+                  if(!strncmp(p->atnam_raw, q->atnam_raw, 4))
+                  {
+                     r->next = q->next;
+                     free(q);
+                     q=r;
+                     p->altpos = ' ';
+                     break;
+                  }
+                  r=q;
+               }
+            }
+         }
+      }
+   }
+   return(pdb);
+}
+
 
