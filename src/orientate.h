fec0bbd0b133a63c4cef8f1e22b38439b9c65a66
AndrewCRMartin <andrew@bioinf.org.uk>
2015-08-24 18:06:31 +0100

    orientate.h - New prototypes

--- src/orientate.h
+++ src/orientate.h
@@ -2,10 +2,14 @@
 #define ORIENTATE_H
 
 BOOL OrientatePDB(PDB *pdb, PDB *res1_start, PDB *res1_next);
+BOOL OrientateN_PDB(PDB *pdb, PDB *prev, PDB *start, PDB *next);
+BOOL OrientateCO_PDB(PDB *pdb, PDB *start, PDB *next);
 void RotateToXZ(PDB *pdb, VEC3F *n);
 void RotateToX(PDB *pdb, VEC3F *n);
 void RotateToXY(PDB *pdb, VEC3F *c_beta);
 REAL TrueAngle(REAL opp, REAL adj);
-BOOL FindNCACAtoms(PDB *res1_start, PDB *stop, VEC3F *c_alpha, VEC3F *c_beta, VEC3F *n);
+BOOL FindNCACBAtoms(PDB *res1_start, PDB *stop, VEC3F *c_alpha, VEC3F *c_beta, VEC3F *n);
+BOOL FindCACOAtoms(PDB *res1_start, PDB *stop, VEC3F *c_alpha, VEC3F *c_beta, VEC3F *n);
+BOOL FindCNCAAtoms(PDB *res0_start, PDB *res1_start, PDB *stop, VEC3F *c, VEC3F *n, VEC3F *c_alpha);
 
 #endif
