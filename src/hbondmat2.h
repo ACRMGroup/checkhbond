1db991b9bc42a6f9f7f93779bec02c1440022e45
AndrewCRMartin <andrew@bioinf.org.uk>
2015-08-24 18:05:57 +0100

    hbondmat2.h - hbond matrix definitions

---
+++ src/hbondmat2.h
@@ -0,0 +1,19 @@
+#define DIV_PER_ANGSTROM 2
+#define MAXCAHBDIST      15
+
+#define MAXSIZE (2*MAXCAHBDIST*DIV_PER_ANGSTROM)
+#define DIV ((REAL)1/DIV_PER_ANGSTROM)
+#define OFFSET (MAXSIZE/2)
+#define MAXBUFF 300
+#define PDBEXT ".ent"
+#define DOMAINLOC "/acrm/data/dompdb/"
+#define PDBLOC "/data/pdb/pdb"
+#define PGPFILE "Explicit.pgp"
+#define COORD_2_GRID(x,y) (x) = ((int)(y/DIV) + OFFSET)
+#define GRID_2_COORD(x,y) (y) = ((REAL)(x - OFFSET) * DIV)
+#define VALIDGRIDCOORDS(x, y, z)  \
+ (((x) < MAXSIZE) && ((x) >= 0) && \
+  ((y) < MAXSIZE) && ((y) >= 0) &&  \
+  ((z) < MAXSIZE) && ((z) >= 0))
+#define GRIDSPACING ((REAL)1/DIV_PER_ANGSTROM)
+#define RESOL 2.5
